2a40029f53bb2f5a03240672d1a143e7070f229c
GambrinusUp <kotanffas@gmail.com>
2022-11-30 14:30:31 +0700

    Добавлены модели данных и enum`ы -DishBasketDto, DishDto, DishPagedListDto, LoginCredentials, OrderCreateDto, OrderDto, OrderInfoDto, PageInfoModel, UserDto, UserEditModel, UserRegisterModel -DishCategory, DishSorting, OrderStatus, Gender

---
+++ FoodDelivery/Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace FoodDelivery.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
